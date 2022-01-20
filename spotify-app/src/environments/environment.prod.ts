76dba4cfb3444badd5ba8b779615278fa06fb50a
MathieuEnault <enault.mathieu2@gmail.com>
2022-01-20 19:55:53 +0100

    build created

--- spotify-app/src/environments/environment.prod.ts
+++ spotify-app/src/environments/environment.prod.ts
@@ -1,5 +1,28 @@
 export const environment = {
   clientId: '9007fa782c1d4b429fb7902f98becbd4',
   clientSecret: 'df942142510b44fda6528d6bc5c012a8',
+  uri: 'http://localhost:4200/',
+  authorizeUri: 'https://accounts.spotify.com/authorize',
+  SCOPES: [
+    'ugc-image-upload',
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'app-remote-control',
+    'streaming',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-follow-modify',
+    'user-follow-read',
+    'user-library-modify',
+    'user-library-read',
+    'user-read-email',
+    'user-read-private'
+  ],
   production: true
 };
